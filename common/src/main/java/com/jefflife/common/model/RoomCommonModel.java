3aa79ef56f47896f796d4c4d04cbea99dde0898c
ch3224bin <ahrocksil@gmail.com>
2023-10-22 22:44:37 +0900

    feat(common): Room, WayOut 모델을 record에서 class로 변경
    - record를 사용하여 상속대신 구상하는 것도 괜찮음
    - 하지만, 코드 줄수가 길어지고 필드가 추가될 때마다 getter를 추가해줘야 하는 단점이 있음

--- common/src/main/java/com/jefflife/common/model/RoomCommonModel.java
+++ common/src/main/java/com/jefflife/common/model/RoomCommonModel.java
@@ -1,13 +1,24 @@
 package com.jefflife.common.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
 import java.util.List;
 
-public record RoomCommonModel(
-    long id,
-    String summary,
-    String description,
-    List<WayOutCommonModel> wayOuts,
-    String exitString
-) {
+@Getter
+@EqualsAndHashCode(of = "id")
+public class RoomCommonModel {
+    private final long id;
+    private final String summary;
+    private final String description;
+    private final List<WayOutCommonModel> wayOuts;
+    private final String exitString;
 
+    public RoomCommonModel(long id, String summary, String description, List<WayOutCommonModel> wayOuts, String exitString) {
+        this.id = id;
+        this.summary = summary;
+        this.description = description;
+        this.wayOuts = wayOuts;
+        this.exitString = exitString;
+    }
 }
--- common/src/main/java/com/jefflife/common/model/WayOutCommonModel.java
+++ common/src/main/java/com/jefflife/common/model/WayOutCommonModel.java
@@ -1,4 +1,24 @@
 package com.jefflife.common.model;
 
-public record WayOutCommonModel(long id, long roomId, long nextRoomId, String direction, boolean isShow, boolean isLocked) {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+public class WayOutCommonModel {
+    private final long id;
+    private final long roomId;
+    private final long nextRoomId;
+    private final String direction;
+    private final boolean isShow;
+    private final boolean isLocked;
+
+    public WayOutCommonModel(long id, long roomId, long nextRoomId, String direction, boolean isShow, boolean isLocked) {
+        this.id = id;
+        this.roomId = roomId;
+        this.nextRoomId = nextRoomId;
+        this.direction = direction;
+        this.isShow = isShow;
+        this.isLocked = isLocked;
+    }
 }
