4003ad2edce7ff77ddd8e4e01ece18bbb60ac9b1
ch3224bin <ahrocksil@gmail.com>
2023-10-22 22:52:13 +0900

    refactor(common): Room, WayOut 모델을 class에서 interface로 변경

--- common/src/main/java/com/jefflife/common/model/RoomCommonModel.java
+++ common/src/main/java/com/jefflife/common/model/RoomCommonModel.java
@@ -1,24 +1,11 @@
 package com.jefflife.common.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-@EqualsAndHashCode(of = "id")
-public class RoomCommonModel {
-    private final long id;
-    private final String summary;
-    private final String description;
-    private final List<WayOutCommonModel> wayOuts;
-    private final String exitString;
-
-    public RoomCommonModel(long id, String summary, String description, List<WayOutCommonModel> wayOuts, String exitString) {
-        this.id = id;
-        this.summary = summary;
-        this.description = description;
-        this.wayOuts = wayOuts;
-        this.exitString = exitString;
-    }
+public interface RoomCommonModel {
+    long getId();
+    String getSummary();
+    String getDescription();
+    List<WayOutCommonModel> getWayOuts();
+    String getExitString();
 }
