e8721203addfad0af7d282642af4b2def0ee4704
ch3224bin <ahrocksil@gmail.com>
2023-10-29 17:02:20 +0900

    feat(common): CommandDataModel 추가

---
+++ common/src/main/java/com/jefflife/common/model/CommandDataCommonModel.java
@@ -0,0 +1,8 @@
+package com.jefflife.common.model;
+
+public interface CommandDataCommonModel {
+    String getTarget();
+    String getPayload();
+    String getAdverb();
+    String getAction();
+}
